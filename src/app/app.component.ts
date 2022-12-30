01f55956f5652ff62486a2aa517975345d75e05c
Cooper Werner <cooperwerner824@gmail.com>
2022-12-29 19:07:23 -0500

    Colors, Styles, and Formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'coop-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'coop-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+	title = 'Cooper Werner';
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{html,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				'coop-background': '#000B2E',
+				'coop-highlight': '#00D7EB',
+				'coop-text-header': '#D3F3FC',
+				'coop-text-paragraph': '#8EACC7',
+				'coop-text-dark': '#000B2E'
+			},
+			fontFamily: {
+				round: ['Nunito', 'Varela Round', 'Trebuchet MS'],
+				square: ['Helvetica', 'sans-serif'],
+				mono: ['Consolas', 'Courier New']
+			}
+		}
+	},
+	plugins: [
+		// require('@tailwindcss/typography'),
+		// require('@tailwindcss/forms'),
+		// require('@tailwindcss/aspect-ratio')
+	]
+};
