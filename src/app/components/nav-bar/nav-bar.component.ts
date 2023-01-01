26440b538831c9ba2cd4f0264b0a6ff4e9a1dc33
Cooper Werner <cooperwerner824@gmail.com>
2023-01-01 14:26:01 -0500

    Landing Page Done

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,6 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { faBars, faWindowClose } from '@fortawesome/free-solid-svg-icons';
-
+import {
+	faBars,
+	faWindowClose,
+	faPhone,
+	faEnvelope,
+	faCode,
+	faFileCode,
+	faLaptopCode,
+	faUser,
+	faMusic,
+	faSchool
+} from '@fortawesome/free-solid-svg-icons';
+import { faBuilding } from '@fortawesome/free-regular-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 @Component({
 	selector: 'coop-nav-bar',
 	templateUrl: './nav-bar.component.html',
@@ -9,6 +21,16 @@ import { faBars, faWindowClose } from '@fortawesome/free-solid-svg-icons';
 export class NavBarComponent implements OnInit {
 	faList = faBars;
 	faWindowClose = faWindowClose;
+	faPhone = faPhone;
+	faEnvelope = faEnvelope;
+	faBuilding = faBuilding;
+	faCode = faCode;
+	faFileCode = faFileCode;
+	faGithub = faGithub;
+	faLaptopCode = faLaptopCode;
+	faUser = faUser;
+	faMusic = faMusic;
+	faSchool = faSchool;
 
 	mobileMenu: HTMLElement | null | undefined;
 	constructor() {}
