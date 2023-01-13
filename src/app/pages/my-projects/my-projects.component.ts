63e42d8d1d6110a67dbc763bad4d490e3040621b
Cooper Werner <cooper@techslice.com>
2023-01-13 15:39:02 -0500

    more fixes

--- src/app/pages/my-projects/my-projects.component.ts
+++ src/app/pages/my-projects/my-projects.component.ts
@@ -26,8 +26,8 @@ export class MyProjectsComponent implements OnInit {
 	readonly projects: project[] = [
 		{
 			mediaType: 'picture',
-			mediaSrc: '../../../assets/imgs/WFC_Circuit.webp',
-			mobileMediaSrc: '../../../assets/imgs/WFC_Circuit_Mobile.webp',
+			mediaSrc: '../../../assets/imgs/WFC_Circuit.png',
+			mobileMediaSrc: '../../../assets/imgs/WFC_Circuit_Mobile.png',
 			title: 'Wave Function Collapse: Procedural Image Generation using Physics.',
 			description:
 				'An implementation of the Wave Function Collapse algorithm in Angular.',
@@ -39,8 +39,8 @@ export class MyProjectsComponent implements OnInit {
 		},
 		{
 			mediaType: 'picture',
-			mobileMediaSrc: '../../../assets/imgs/WFC_Circuit_Mobile.webp',
-			mediaSrc: '../../../assets/imgs/WFC_Circuit.webp',
+			mobileMediaSrc: '../../../assets/imgs/WFC_Circuit_Mobile.png',
+			mediaSrc: '../../../assets/imgs/WFC_Circuit.png',
 			title: 'Western Tech Data Tool: A Simpler way to Visualize Data.',
 			description:
 				'A data visualization and analysis tool built with Python and PyQT.',
@@ -48,8 +48,8 @@ export class MyProjectsComponent implements OnInit {
 		},
 		{
 			mediaType: 'picture',
-			mobileMediaSrc: '../../../assets/imgs/WFC_Circuit_Mobile.webp',
-			mediaSrc: '../../../assets/imgs/WFC_Circuit.webp',
+			mobileMediaSrc: '../../../assets/imgs/WFC_Circuit_Mobile.png',
+			mediaSrc: '../../../assets/imgs/WFC_Circuit.png',
 			title: 'Pi-Day 2022: Three Algorithms to Aproximate Pi.',
 			description: '3 Algoritims to calculate Pi.',
 			codeLink: 'https://github.com/CooperW824/Pi-Day-Algorithms',
@@ -62,8 +62,8 @@ export class MyProjectsComponent implements OnInit {
 		},
 		{
 			mediaType: 'video',
-			mobileMediaSrc: '../../../assets/imgs/WFC_Circuit_Mobile.webp',
-			mediaSrc: '../../../assets/imgs/WFC_Circuit.webp',
+			mobileMediaSrc: '../../../assets/imgs/WFC_Circuit_Mobile.png',
+			mediaSrc: '../../../assets/imgs/WFC_Circuit.png',
 			title: 'OpenVaxx: An Award Ainning, Open-Source, Annonymous, and Secure, Vaccine Passport.',
 			description: 'A Vaccine Passport',
 			codeLink: 'https://github.com/CooperW824/OpenVaxx',
@@ -76,8 +76,8 @@ export class MyProjectsComponent implements OnInit {
 		},
 		{
 			mediaType: 'video',
-			mobileMediaSrc: '../../../assets/imgs/WFC_Circuit_Mobile.webp',
-			mediaSrc: '../../../assets/imgs/WFC_Circuit.webp',
+			mobileMediaSrc: '../../../assets/imgs/WFC_Circuit_Mobile.png',
+			mediaSrc: '../../../assets/imgs/WFC_Circuit.png',
 			title: 'Hobbits USB Reader: A tool to simplify reading from USB devices.',
 			description: 'A USB developer tool',
 			codeLink: 'https://github.com/Mahlet-Inc/hobbits'
