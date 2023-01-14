8e67345006b07fc0ae147747ebb67477d3f6bc59
Cooper Werner <cooperwerner824@gmail.com>
2023-01-14 16:04:02 -0500

    Starting Experience Page

--- src/app/pages/work-experience/work-experience.component.ts
+++ src/app/pages/work-experience/work-experience.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
+
+interface reference {
+	name: string;
+	title: string;
+	email: string;
+}
+
+interface experience {
+	company: string;
+	// title: string;
+	// keyRoles: string[];
+	// skills: string[];
+	// references: reference[]
+}
 
 @Component({
 	selector: 'coop-work-experience',
@@ -7,9 +20,37 @@ import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
 	styleUrls: ['./work-experience.component.css']
 })
 export class WorkExperienceComponent implements OnInit {
-	faLaptopCode = faLaptopCode;
+	jobs: experience[] = [
+		{
+			company: 'TechSlice'
+		},
+		{
+			company: 'Bellese'
+		},
+		{
+			company: 'SMR'
+		}
+	];
 
+	previousButton: string = `${this.jobs[0].company}Button`;
+	previousWorkDisplay: string = `${this.jobs[0].company}Display`;
 	constructor() {}
 
 	ngOnInit(): void {}
+
+	ngAfterViewChecked(): void {
+		let buttonElm = document.getElementById(this.previousButton);
+		buttonElm?.classList.add('selected');
+	}
+
+	open(company: string): void {
+		let button = `${company}Button`;
+		let display = `${company}Display`;
+		console.log(`${button}, ${display}`);
+		let buttonElm = document.getElementById(button);
+		buttonElm?.classList.toggle('selected');
+		buttonElm = document.getElementById(this.previousButton);
+		buttonElm?.classList.toggle('selected');
+		this.previousButton = button;
+	}
 }
